c9276210ea95f8f10920371fffac2eacc4c7d537
zhangdw156 <zhangdw156@gmail.com>
2024-12-03 23:20:59 +0800

    搭建好了前后端框架，均可运行

--- exam-server/src/main/java/com/exam/ExamServerApplication.java
+++ exam-server/src/main/java/com/exam/ExamServerApplication.java
@@ -1,13 +1,13 @@
-package org.llm;
+package com.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class ExamServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ExamServerApplication.class, args);
     }
 
 }
--- exam-server/src/test/java/com/exam/ExamServerApplicationTests.java
+++ exam-server/src/test/java/com/exam/ExamServerApplicationTests.java
@@ -1,10 +1,10 @@
-package org.llm;
+package com.exam;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LlmApplicationTests {
+class ExamServerApplicationTests {
 
     @Test
     void contextLoads() {
