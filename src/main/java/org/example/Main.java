2d53be80b43581649c9ea681523dfa2ff1cd2223
pt <albusrichard666@gmail.com>
2024-12-27 22:59:44 +0800

    prompt类完成了选择判断填空简答的逻辑，并使用buildPrompt函数进行prompt的系统构建； 重构了APIService使得其函数能各司其职，如添加历史消息、parsingJsonString、构建Request等； 使用conversation类处理用户与LLM的交互逻辑，以便后续多个会话的实现；并且添加了反馈机制

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
 package org.example;
 import java.util.Arrays;
 import java.util.List;
-import org.example.AzureAIServiceClient;
-import io.github.cdimascio.dotenv.Dotenv;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         List<String> questionTypes = Arrays.asList("multiple-choice","true-false","fill-in-the-blank","short-answer");
